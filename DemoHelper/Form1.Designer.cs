687a975b60ce7046460e0ac359640d459180c4bb
Ryan Batchelder <ryanbatch@gmail.com>
2014-10-08 20:17:40 -0700

    Initial Form Layout

--- DemoHelper/Form1.Designer.cs
+++ DemoHelper/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DemoHelper
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.CodeClips = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // CodeClips
+            // 
+            this.CodeClips.FormattingEnabled = true;
+            this.CodeClips.Location = new System.Drawing.Point(13, 13);
+            this.CodeClips.Name = "CodeClips";
+            this.CodeClips.Size = new System.Drawing.Size(259, 238);
+            this.CodeClips.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.CodeClips);
+            this.Name = "MainWindow";
+            this.Text = "Demo Helper";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox CodeClips;
     }
 }
 
