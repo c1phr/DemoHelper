a21d1dbb6964a113447c70ca954522b209ab99b5
--replace-all <=>
2014-10-10 11:49:26 -0700

    Last commit was for Step 16

--- DemoHelper/Snippets.cs
+++ DemoHelper/Snippets.cs
@@ -227,7 +227,7 @@ namespace DemoHelper
     catch (DataException /* dex */)
     {
         //Log the error (uncomment dex variable name and add a line here to write a log.
-        ModelState.AddModelError("", ""Unable to save changes. Try again, and if the problem persists see your system administrator."");
+        ModelState.AddModelError("""", ""Unable to save changes. Try again, and if the problem persists see your system administrator."");
     }"
             },
 
